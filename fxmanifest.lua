2c89fb62c844d274874ebab15cdece7fd297280a
Bandit <101681960+IAm-Bandit@users.noreply.github.com>
2025-05-23 02:07:38 +0100

    Create fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,9 @@
+fx_version "cerulean"
+game 'gta5'
+
+name 'bSling'
+author 'the.bandit_'
+description 'FiveM Weapon Sling Script -- https://github.com/IAm-Bandit/FiveM-Sling'
+
+client_script 'cl_main.lua'
+shared_script 'sh_config.lua'
